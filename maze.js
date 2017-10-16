7a306f66b7a8eaa2fc9c946bbb1d4c03e3571450
Jonathan <jonforce52@hotmail.com>
2017-10-16 10:53:56 -0500

    Excercise 1 complete

---
+++ maze.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+						var elem = document.getElementById("boundary1");
+						elem.addEventListener('mouseover',changeColour);
+						function changeColour(){
+						elem.classList.add("youlose");
+}
+	}
