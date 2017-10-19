5119c4e0f19d58998887892c012fb01fd39e9c03
Jonathan <jonforce52@hotmail.com>
2017-10-18 20:35:34 -0500

    Excercise 2 Complete

--- maze.js
+++ maze.js
@@ -1,7 +1,11 @@
 window.onload=function(){
-						var elem = document.getElementById("boundary1");
-						elem.addEventListener('mouseover',changeColour);
-						function changeColour(){
-						elem.classList.add("youlose");
-}
-	}
+						var query = document.querySelectorAll(".boundary");		
+						for(var i =0;i < query.length ; i++){
+							query[i].addEventListener('mouseover',changeColour); 
+							function changeColour(){
+							for(let index = 0; index<query.length-1; index++){
+								query[index].setAttribute("class","boundary youlose");
+							 }
+						}
+					}
+				}
